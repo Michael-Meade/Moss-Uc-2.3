d0b5c11266d4c1501c8db1f96c441bbc266c9acc
Michael-Meade <mameade@utica.edu>
2020-11-07 18:43:58 -0500

    Source updated at 2020-11-07 18:43:58 -0500.

--- rakefile.rb
+++ rakefile.rb
@@ -9,8 +9,19 @@ task :commit, :message  do |t, args|
   if message==nil
     message = "Source updated at #{Time.now}."
   end
+  old     = JSON.parse(File.read("config.json"))
+  newz    = JSON.parse(File.read("config.json"))
+  keys = [ "api-key", "youtube", "X-CMC_PRO_API_KEY"]
+  newz.each do |key, value|
+    if keys.include?(key)
+      newz[key] = "key"
+    end
+  end
+  File.open("config.json", "w") { |file| file.write(newz.to_json) }
   system "git add ."
   system "git commit -a -m \"#{message}\""
+  system "git push -u origin master"
+  File.open("config.json", "w") { |file| file.write(old.to_json) }
 end
 
 
@@ -24,7 +35,7 @@ task :deploy, :message do |t, args|
   puts "pushing to remote:"
   system "git remote -v"
   Rake::Task[:push].execute 
-
+  system "git push -u origin master"
   
 end
 
